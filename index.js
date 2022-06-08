b11e15e9151c6eafe87c8621531251430d1690f3
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-08 10:10:01 +0300

    commit tree 53951c47d12e3b5aedd1215773bfdaa5f5404753

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000 / 100;
+}
+
+const square = (multiply) => multiply * multiply
+console.log(square(5))
+
+const add = (num1,num2) => (num1 + num2)
+
+console.log(add(3,4))
